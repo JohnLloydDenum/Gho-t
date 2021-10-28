6a87de4378f8b37cdb01127f5aa5a705a6c2cf80
JohnLloydDenum <69796424+JohnLloydDenum@users.noreply.github.com>
2021-10-29 00:17:51 +0800

    Add files via upload

--- component.php
+++ component.php
@@ -8,8 +8,16 @@ function inputElement($icon, $placeholder, $name, $value){
                             <div class=\"input-group-text bg-warning\">$icon</div>
                         </div>
                         <input type=\"text\" name='$name' value='$value' autocomplete=\"off\" placeholder='$placeholder' class=\"form-control\" id=\"inlineFormInputGroup\" placeholder=\"Username\">
+                        
                     </div>
     
     ";
     echo $ele;
+}
+
+function buttonElement($btnid, $styleclass, $text, $name, $attr){
+    $btn = "
+    <button name='$name' '$attr' class='$styleclass' id='$btnid' > $text </button>
+    ";
+    echo $btn;
 }
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once ("../HTML/components/component.php")
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,13 +12,13 @@ require_once ("../HTML/components/component.php")
 
     <script src="https://kit.fontawesome.com/a9428e25d8.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
 
 </head>
 <body>
     <main>
         <div class="container text-center">
-            <h1 class="py-4 bg-dark text-light rounded"> <i class="fas fa-swatchbook"></i> Sales Inventory </h1>
+            <h1 class="py-5 bg-dark text-light rounded"> <i class="fas fa-swatchbook"></i> Sales Inventory </h1>
         <div class="d-flex justify-content-center">
             <form action=""method="post" class="w-60">
             <div class="py-2"> 
@@ -26,9 +27,15 @@ require_once ("../HTML/components/component.php")
         
 
             <div class="pt-2">
+            <?php inputElement("<i class='fas fa-book'></i>","Product Name", "fproduct_name",""); ?>  
+            </div>
+            <div class="pt-2">
+            <?php inputElement("<i class='fas fa-book'></i>","Quantity", "quantity",""); ?>  
+            </div>
+            <div class="pt-2">
             <?php inputElement("<i class='fas fa-book'></i>","First Name", "first_name",""); ?>  
             </div>
-             <div class="pt-3">
+             <div class="pt-2">
              <?php inputElement("<i class='fas fa-book'></i>","Last Name", "last_name",""); ?>
              </div>
              <div class="row">
@@ -40,14 +47,50 @@ require_once ("../HTML/components/component.php")
              </div>
 
         </div>
+        <div class="d-flex justify-content-center">
+            
+            <?php buttonElement("btn-create", "btn btn-success", "<i class='fas fa-plus'> </i>", "create", "dat-toggle='tooltip'data-placement='bottom' title='Create'"); ?>
+            <?php buttonElement("btn-read", "btn btn-primary", "<i class='fas fa-sync'> </i>", "read", "dat-toggle='tooltip'data-placement='bottom' title='Read'"); ?>
+            <?php buttonElement("btn-update", "btn btn-light border", "<i class='fas fa-pen-alt'> </i>", "update", "dat-toggle='tooltip'data-placement='bottom' title='Update'"); ?>
+            <?php buttonElement("btn-delete", "btn btn-danger", "<i class='fas fa-trash-alt'> </i>", "delete", "dat-toggle='tooltip'data-placement='bottom' title='Delete'"); ?>
+            
+        </div>
 
         </form>
+        </div>
+        <div class="d-flex table-data">
+            <table class="table table-striped table-dark">
+            <thead class="table-dark"> 
+                <tr>
+                    <th> Product ID </th>
+                    <th> Product Name</th>
+                    <th> Quantity </th>
+                    <th> First Name </th>
+                    <th> Last Name </th>
+                    <th> Date </th>
+                    <th> Price </th>
+                    <th> Edit </th>
+                </tr>
+            </thead>
+            <tbody id="tbody">
+                <tr>
+                    <td>1</td>
+                    <td>Alfonso Platinum</td>
+                    <td>3</td>
+                    <td>JohnLloyd</td>
+                    <td>Denum</td>
+                    <td>10/28/21</td>
+                    <td>1500</td>
+                    <td> <i class="fas fa-edit btnedit"> </i> </td>
+                </tr>
+            </tbody>
+            </table>
         </div>
         </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>   
 
-
+    </link>
 </body>
 </html>
